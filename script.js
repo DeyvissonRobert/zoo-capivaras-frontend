179da8700ba668e700c0594709666a13df1d624b
DeyvissonRobert <robert.designerx@gmail.com>
2024-09-29 19:40:35 -0300

    Estilização da Lista de Capivaras

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const API_URL = 'http://localhost:3000/capivaras';
+
+const capivaraList = document.getElementById('capivara-list');
+const searchInput = document.getElementById('search-input');
+
+
+
+
+
+
