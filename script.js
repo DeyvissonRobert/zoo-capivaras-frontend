159f1489b56dfe6f4a9408454330c5131377a199
DeyvissonRobert <robert.designerx@gmail.com>
2024-09-29 21:15:15 -0300

    Função de exibir capivaras da lista na tela

--- script.js
+++ script.js
@@ -20,4 +20,30 @@ async function fetchCapivaras() {
 }
 
 
+function displayCapivaras(capivaras) {
+    capivaraList.innerHTML = '';
+
+    capivaras.forEach(capivara => {
+        const capivaraItem = document.createElement('li');
+        capivaraItem.classList.add('capivara-item');
+
+        capivaraItem.innerHTML = `
+            <div class="capivara-info">
+                <strong>ID:</strong> ${capivara.id} <br>
+                <strong>Nome:</strong> ${capivara.nome} <br>
+                <strong>Idade:</strong> ${capivara.idade} anos <br>
+                <strong>Peso:</strong> ${capivara.peso} kg <br>
+                <strong>Habitat:</strong> ${capivara.habitat}
+                <div class="capivara-actions">
+                    <button class="edit-btn" onclick="editCapivara(${capivara.id})"><span class="material-symbols-outlined">edit</span></button>
+                    <button class="delete-btn" onclick="deleteCapivara(${capivara.id})"><span class="material-symbols-outlined">delete_forever</span></button>
+                </div>
+            </div>
+            
+        `;
+
+        capivaraList.appendChild(capivaraItem);
+    });
+}
+
 fetchCapivaras();
